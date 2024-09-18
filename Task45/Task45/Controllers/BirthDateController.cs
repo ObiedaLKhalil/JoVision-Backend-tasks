4feb5693958bda43f2bf384e2218190b23557655
ObiedaLKhalil <olkhalil2024@gmail.com>
2024-09-18 10:38:03 +0300

    end Task45

---
+++ Task45/Task45/Controllers/BirthDateController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Task45.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BirthDateController : ControllerBase
+    {
+        // GET: api/birthdate
+        [HttpGet]
+        public IActionResult GetAge([FromQuery] string name, [FromQuery] int? years, [FromQuery] int? months, [FromQuery] int? days)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "anonymous";
+            }
+
+            if (!years.HasValue || !months.HasValue || !days.HasValue)
+            {
+                return Ok($"Hello {name}, I can't calculate your age without knowing your birthdate!");
+            }
+
+            var birthdate = new DateTime(years.Value, months.Value, days.Value);
+            var today = DateTime.Today;
+           
+
+            int age = today.Year - birthdate.Year;
+            if (birthdate > today.AddYears(-age)) age--;
+
+            return Ok($"Hello {name}, your age is {age}.");
+        }
+    }
+}
---
+++ Task45/Task45/Controllers/GreeterController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiControllerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreeterController : ControllerBase
+    {
+        // GET: api/greeter
+        [HttpGet]
+        public IActionResult Greet([FromQuery] string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "anonymous";
+            }
+
+            var message = $"Hello {name}";
+            return Ok(message);
+        }
+    }
+}
